85e038fcdb093ebecdde86fd6ce54dd41b0cf80e
hamdi <javs_shao@163.com>
2018-12-12 20:28:22 +0800

    初始化方法

--- run.py
+++ run.py
@@ -2,3 +2,9 @@
 import unittest
 from selenium import webdriver
 from bs4 import BeautifulSoup
+
+
+class douyuSelenium(unittest.TestCase):
+    # 初始化方法
+    def setUp(self):
+        self.driver = webdriver.Chrome()
